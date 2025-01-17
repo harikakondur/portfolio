6b7f7d993740a17901d8d6bd3971089239d94df3
Harika <harikakondur3@gmail.com>
2025-01-17 15:55:48 -0700

    using contentful api

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,5 +1,23 @@
+import { useEffect, useState } from "react";
+
+import getEntries from "../api"
 
 export default function Projects(){
+
+    const [projects, setProjects] = useState([]);
+
+    const fetchData = async () => {
+        try {
+          const response = await getEntries();
+          setProjects(response.items);
+        } catch (error) {
+          console.error("Error fetching projects:", error);
+        }
+      };
+
+      useEffect(() => {
+        fetchData();
+      }, []);
 
     return(
         <div>Projects</div>
