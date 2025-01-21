2185d3b364704c72bab98575b0f0f32b155abbec
Harika <harikakondur3@gmail.com>
2025-01-20 23:51:38 -0700

    fixing routing

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename='/portfolio'>
     <App />
   </BrowserRouter>
 );
