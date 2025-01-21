d6390f1e527af572e8d5b5d0a47e51067cc44b29
Harika <harikakondur3@gmail.com>
2025-01-20 23:51:38 -0700

    fixing routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router';
+import { Route, Routes } from 'react-router';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 
 function App() {
   return (
-    <BrowserRouter basename="/portfolio">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
-    </BrowserRouter>
-  );
+=  );
 }
 
 export default App;
