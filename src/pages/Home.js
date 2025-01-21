876502d9adbee1a4d3dab1c337d304a9fb78176b
Harika <harikakondur3@gmail.com>
2025-01-20 23:03:48 -0700

    adding homepage links

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,16 +12,7 @@ export default function Home(){
     return(
         <div className="bg-light-purple h-screen relative flex flex-col justify-center items-center">
 
-            {/* <div className="fixed top-0 right-0 h-full bg-purple-100 flex flex-col justify-start items-center shadow-md p-16 space-y-2">
-                <a href="https://github.com/harikakondur" target="_blank" class="underline italic">GitHub</a>
-                <a href="https://docs.google.com/document/d/e/2PACX-1vQ1xywPoVJDAbpk9yzHt6W76IeL8AiyBUa2CWROh52iq7LhrDCeHpVvbdEFhtq_ysxEgNp_366vqLCl/pub" target="_blank" class="underline italic">Resume</a>
-                <a href="https://www.linkedin.com/in/harika-kondur/" target="_blank" class="underline italic">LinkedIn</a>
-            </div>
-
-            <div className="text-center text-gray-800">
-                <h1 className="text-4xl font-bold"> Homepage</h1>
-                <p className="text-lg mt-2">Hi my name is Harika Kondur</p>
-            </div> */}
+            
 
             <div className="text-6xl font-mono tracking-widest">
                 <span>harika </span> 
@@ -29,7 +20,16 @@ export default function Home(){
                 <span>r</span>
             </div>
 
-            <div className="mt-4 font-mono italic p-2" onClick={() => navigate('/projects')}>projects</div>
+            <div className="mt-4 font-mono italic p-2 items-center text-md">
+                <div  onClick={() => navigate('/projects')}>projects</div>
+                <div><a href="https://github.com/harikakondur" target="_blank">github</a></div>
+                <div><a href="https://docs.google.com/document/d/e/2PACX-1vQ1xywPoVJDAbpk9yzHt6W76IeL8AiyBUa2CWROh52iq7LhrDCeHpVvbdEFhtq_ysxEgNp_366vqLCl/pub" target="_blank" >resume</a></div>
+                <div><a href="https://www.linkedin.com/in/harika-kondur/" target="_blank">linkedin</a></div>
+                
+
+            </div>
+
+            
             
         </div>
         
