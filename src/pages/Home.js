39dd0cd7983748784e08b1cc3e8ed59a0b5e9d7c
Harika <harikakondur3@gmail.com>
2025-01-24 15:25:31 -0700

    updating  link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -23,7 +23,7 @@ export default function Home(){
             <div className="mt-4 font-mono italic p-2 items-center text-md">
                 <div  onClick={() => navigate('/projects')}>projects</div>
                 <div><a href="https://github.com/harikakondur" target="_blank">github</a></div>
-                <div><a href="https://docs.google.com/document/d/e/2PACX-1vQ1xywPoVJDAbpk9yzHt6W76IeL8AiyBUa2CWROh52iq7LhrDCeHpVvbdEFhtq_ysxEgNp_366vqLCl/pub" target="_blank" >resume</a></div>
+                <div><a href="https://docs.google.com/document/d/e/2PACX-1vTueuYxIGag7Q6V4_dJTP4plmAIJGgCYeT218Uc4SM_7crc6ZVSQT5QzpxHQ7jUPWT0eyBBYT0wcJpJ/pub" target="_blank" >resume</a></div>
                 <div><a href="https://www.linkedin.com/in/harika-kondur/" target="_blank">linkedin</a></div>
                 
 
