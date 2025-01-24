cac43dfd6dbc1e80cc3b6cb372be91cfed0c80ad
Harika <harikakondur3@gmail.com>
2025-01-24 13:28:08 -0700

    adding homepage to package.json

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Projects from './pages/Projects';
 
 function App() {
   return (
-      <Routes>
+      <Routes >
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
