eecbe79a0295c31aaf4ca9a221f61fa70fb2298e
Harika <harikakondur3@gmail.com>
2025-01-26 20:58:44 -0700

    updating resume

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,10 +5,6 @@ export default function Home(){
 
     const navigate = useNavigate()
 
-
-
-    
-
     return(
         <div className="bg-light-purple h-screen relative flex flex-col justify-center items-center">
 
@@ -23,7 +19,7 @@ export default function Home(){
             <div className="mt-8 font-mono italic p-2 items-center text-center text-md space-y-2">
                 <div  className= "hover:text-lime-600" onClick={() => navigate('/projects')}>projects</div>
                 <div className= "hover:text-lime-600"><a href="https://github.com/harikakondur" target="_blank">github</a></div>
-                <div className= "hover:text-lime-600"><a href="https://docs.google.com/document/d/e/2PACX-1vTueuYxIGag7Q6V4_dJTP4plmAIJGgCYeT218Uc4SM_7crc6ZVSQT5QzpxHQ7jUPWT0eyBBYT0wcJpJ/pub" target="_blank" >resume</a></div>
+                <div className= "hover:text-lime-600"><a href="https://docs.google.com/document/d/e/2PACX-1vTtEgxaOa7EwZEfFHwbpqGA_n-XLpu8kYJe_oB7gJ_wtuzf0NyK-_orOXfsqhKNp_MCqZV5-eYYSKGj/pub" target="_blank" >resume</a></div>
                 <div className= "hover:text-lime-600"><a href="https://www.linkedin.com/in/harika-kondur/" target="_blank">linkedin</a></div>
                 
 
