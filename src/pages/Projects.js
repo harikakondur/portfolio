90205223b2b6887fcb2ea70c95e42430c706061e
Harika <harikakondur3@gmail.com>
2025-01-26 18:18:13 -0700

    fixing project urls

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -34,7 +34,11 @@ export default function Projects() {
                 <div className="text-2xl font-bold">
                   {project.fields.projectName}
                   {project.fields.url && (
-                      <a href={project.fields.url}  target="_blank" rel="noopener noreferrer" className="inline-block">
+                      <a href={
+                        project.fields.url.startsWith("http")
+                          ? project.fields.url
+                          : `https://${project.fields.url}`
+                      } target="_blank" rel="noopener noreferrer" className="inline-block">
                         <ArrowUpRightIcon className="h-7 hover:text-lime-500 align-bottom -mb-1" />
                       </a>
                     
